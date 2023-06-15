ba63e315e4b7cd06b73624005605bd4feb6e1db5
Khushi <92801019+KhushbooHamid@users.noreply.github.com>
2023-06-15 14:37:00 +0530

    Create accessModifier.java

---
+++ accessModifier.java
@@ -0,0 +1,24 @@
+class abc
+{
+    public int a=1;
+    protected int b=2;
+     int c=3;
+    private int d=4;
+}
+class xyz extends abc
+{
+    public static void main(String []args)
+    {
+        abc I=new abc();
+        System.out.println(I.a);
+    
+        System.out.println(I.b);
+
+        System.out.println(I.c);
+
+  // System.out.println(I.d); private
+          System.out.println("khushboo hamid");
+
+
+    }
+}
