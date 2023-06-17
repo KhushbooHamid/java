9f58c08c53d51a1c4885b78e3dfc97ef8789b153
Khushi <92801019+KhushbooHamid@users.noreply.github.com>
2023-06-17 11:47:10 +0530

    Create Simple.java

---
+++ Simple.java
@@ -0,0 +1,9 @@
+package mypack;  
+public class Simple{  
+ public static void main(String args[]){  
+    System.out.println("Welcome to package");  
+   }  
+}
+
+//to compile use javac -d . Simple.java
+//to run use java mypack.Simple
