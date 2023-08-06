7cebfb1c6188c7221237c3458e70e4187098f697
khushi <khushboohamid786@gmail.com>
2023-08-06 12:35:40 +0530

    updated

--- FileHandlingWriting.java
+++ FileHandlingWriting.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class FileHandlingWriting {
     public static void main(String[] args) {
         try{
-            FileWriter w=new FileWriter("first.txt");
+            FileWriter w=new FileWriter("abc.txt");//creates new file if file doesnt exist already and if exists overwrites
             w.write("i am khush");
             w.close();
             System.out.println("written successfully");
